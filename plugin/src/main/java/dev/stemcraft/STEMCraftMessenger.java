969acf7ea55df04de2fd38db5ff97acd7f5626b9
James Collins <james.collins@outlook.com.au>
2025-11-22 12:02:34 +1000

    fix incorrect methods called

--- plugin/src/main/java/dev/stemcraft/STEMCraftMessenger.java
+++ plugin/src/main/java/dev/stemcraft/STEMCraftMessenger.java
@@ -9,19 +9,19 @@ public class STEMCraftMessenger {
     }
 
     public void info(CommandSender sender, String message, Throwable ex, String... placeholders) {
-        STEMCraftAPI.api().messenger().log(sender, message, ex, placeholders);
+        STEMCraftAPI.api().messenger().info(sender, message, ex, placeholders);
     }
 
     public void warn(CommandSender sender, String message, Throwable ex, String... placeholders) {
-        STEMCraftAPI.api().messenger().log(sender, message, ex, placeholders);
+        STEMCraftAPI.api().messenger().warn(sender, message, ex, placeholders);
     }
 
     public void error(CommandSender sender, String message, Throwable ex, String... placeholders) {
-        STEMCraftAPI.api().messenger().log(sender, message, ex, placeholders);
+        STEMCraftAPI.api().messenger().error(sender, message, ex, placeholders);
     }
 
     public void success(CommandSender sender, String message, Throwable ex, String... placeholders)  {
-        STEMCraftAPI.api().messenger().log(sender, message, ex, placeholders);
+        STEMCraftAPI.api().messenger().success(sender, message, ex, placeholders);
     }
 
 }
