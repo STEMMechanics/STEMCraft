f9aca9396fdecf7df8fc8c707f3b20097c179418
James Collins <james.collins@outlook.com.au>
2026-04-23 21:19:09 +1000

    minigame fixes

--- plugin/src/test/java/dev/stemcraft/minigame/tntrun/TntRunArenaHandlerTest.java
+++ plugin/src/test/java/dev/stemcraft/minigame/tntrun/TntRunArenaHandlerTest.java
@@ -22,7 +22,6 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
 import java.util.List;
-import java.util.UUID;
 import java.util.function.Predicate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
