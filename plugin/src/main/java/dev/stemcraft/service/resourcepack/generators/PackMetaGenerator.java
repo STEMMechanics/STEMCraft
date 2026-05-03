bb6fc46f16beecc7e2c2ee0e6d541b6c4676796d
Project_HSI <projecthsi@outlook.com>
2026-05-04 09:30:31 +1000

    change pack metadata version generation
    adds in:
    - pack_format
    - min_format
    - max_format
    - supported_formats
    
    Note that the default max is "84". This is for future proofing as our use of 1.21.11 resource packs is compatible with 26.1 resource packs. ("84" is for 26.1 resource packs)
    
    Signed-off-by: Project_HSI <projecthsi@outlook.com>

--- plugin/src/main/java/dev/stemcraft/service/resourcepack/generators/PackMetaGenerator.java
+++ plugin/src/main/java/dev/stemcraft/service/resourcepack/generators/PackMetaGenerator.java
@@ -1,6 +1,7 @@
 package dev.stemcraft.service.resourcepack.generators;
 
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import dev.stemcraft.api.STEMCraftAPI;
 import dev.stemcraft.api.config.ConfigSection;
@@ -8,6 +9,7 @@ import dev.stemcraft.api.config.ConfigSectionView;
 import dev.stemcraft.api.service.resourcepack.ResourcePackService;
 import dev.stemcraft.api.service.resourcepack.generator.ResourcePackGenerator;
 import dev.stemcraft.exception.ResourcePackGeneratorException;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.nio.file.Files;
@@ -17,7 +19,8 @@ import java.nio.file.Path;
  * Generates the pack.mcmeta file for a Minecraft resource pack.
  */
 public class PackMetaGenerator extends ResourcePackGenerator {
-    private static final int DEFAULT_PACK_FORMAT = 64;
+    private static final int DEFAULT_MIN_PACK_VERSION = 64;
+    private static final int DEFAULT_MAX_PACK_VERSION = 84;
 
     /**
      * Constructs a PackMetaGenerator with the given STEMCraftAPI instance.
@@ -41,7 +44,7 @@ public class PackMetaGenerator extends ResourcePackGenerator {
         try {
             JsonObject root = new JsonObject();
             JsonObject pack = new JsonObject();
-            pack.addProperty("pack_format", config.getInt("pack_format", DEFAULT_PACK_FORMAT));
+            addPackVersionToMetadata(config, pack);
             pack.addProperty("description", config.getString("description", "A STEMCraft Resource Pack"));
             root.add("pack", pack);
 
@@ -54,5 +57,43 @@ public class PackMetaGenerator extends ResourcePackGenerator {
         } catch (Exception e) {
             throw new ResourcePackGeneratorException("Failed to write pack.mcmeta", e);
         }
+    }
+
+    /**
+     * <p>Adds Minecraft Pack Version information to a JSON Object.</p>
+     *
+     * @param config The configuration file being used to generate the resource pack.
+     * @param packJson The Pack JSON Object to add the version information to.
+     *
+     * @author ProjectHSI
+     */
+    private void addPackVersionToMetadata(@NotNull final ConfigSectionView config, @NotNull final JsonObject packJson) {
+        final int minPackFormat = config.getInt("min_pack_format", DEFAULT_MIN_PACK_VERSION);
+        final int maxPackFormat = config.getInt("max_pack_format", DEFAULT_MAX_PACK_VERSION);
+        packJson.addProperty("min_format", minPackFormat);
+        packJson.addProperty("max_format", maxPackFormat);
+
+        if (minPackFormat < 65) {
+            addLegacyPackVersionToMetadata(packJson, minPackFormat, maxPackFormat);
+        }
+    }
+
+    /**
+     * <p>Adds Minecraft Pack Version information to a JSON Object.</p>
+     *
+     * @param packJson The Pack JSON Object to add the version information to.
+     * @param minPackFormat The minimum pack format of the resource pack being generated.
+     * @param maxPackFormat The maximum pack format of the resource pack being generated.
+     *
+     * @author ProjectHSI
+     */
+    private void addLegacyPackVersionToMetadata(@NotNull final JsonObject packJson,
+                                                final int minPackFormat, final int maxPackFormat) {
+        packJson.addProperty("pack_format", minPackFormat);
+
+        final JsonArray supportedFormats = new JsonArray();
+        supportedFormats.add(minPackFormat);
+        supportedFormats.add(maxPackFormat);
+        packJson.add("supported_formats", supportedFormats);
     }
 }
